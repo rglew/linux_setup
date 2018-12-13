08c17b9fa333427576970d6b9160f54825adf3f8
Richard Glew <richard.glew@anz.com>
2018-12-13 15:38:03 +1100

    Initial repo setup

---
+++ git_aliases/git-config.sh
@@ -0,0 +1,4 @@
+git config --global user.name "Richard Glew"
+git config --global user.email "<<email goes here>>"
+git config --global user.signingkey "<key goes here>"
+git config --global commit.gpgsign "true"
